8c8011a6994faad15435846627ba8cb1e8bedb55
DESKTOP-C2UO9L3\ucreate-19 <jaspal@ucreate.co.in>
2019-09-05 17:19:31 +0530

    public files

---
+++ public/bootstrap/js/emoji.js
@@ -0,0 +1,16 @@
+var emojies = [
+  { key: '😄', emoji: 'smile' },
+  { key: '😊', emoji: 'blush' },
+  { key: '😏', emoji: 'smirk' },
+  { key: '☺️', emoji: 'relaxed' },
+  { key: '😍', emoji: 'heart_eyes' },
+  { key: '😘', emoji: 'kissing_heart' },
+  { key: '😁', emoji: 'grin' },
+  { key: '😉', emoji: 'wink' },
+  { key: '😛️', emoji: 'stuck_out_tongue' },
+  { key: '😴 ', emoji: 'sleeping' },
+  { key: '😟', emoji: 'worried' },
+  { key: '😮', emoji: 'open_mouth' },
+  { key: '😕', emoji: 'confused' },
+  {key:'👍', emoji:'thumbsup'},{key:'👎', emoji:'thumbsdown'},{key:'👌', emoji:'ok_hand'},{key:'👊', emoji:'punch'},{key:'👊', emoji:'facepunch'},{key:'✊', emoji:'fist'},{key:'✌️', emoji:'v'},{key:'👋', emoji:'wave'},{key:'✋', emoji:'hand'},{key:'✋', emoji:'raised_hand'},{key:'👐', emoji:'open_hands'},{key:'☝️', emoji:'point_up'},{key:'👇', emoji:'point_down'},{key:'👈', emoji:'point_left'},{key:'👉', emoji:'point_right'},{key:'🙌', emoji:'raised_hands'},{key:'🙏', emoji:'pray'},{key:'👆', emoji:'point_up_2'},{key:'👏', emoji:'clap'},{key:'💪', emoji:'muscle'},{key:'🤘', emoji:'metal'},{key:'🖕', emoji:'fu'},{key:'🚶', emoji:'walking'},{key:'🏃', emoji:'runner'},{key:'👫', emoji:'couple'},{key:'👪', emoji:'family'},{key:'🙇', emoji:'bow'},{key:'👦', emoji:'boy'},{key:'👩', emoji:'woman'},{key:'😾', emoji:'pouting_cat'},{key:'🐼', emoji:'panda_face'},{key:'💋', emoji:'kiss'}
+];
